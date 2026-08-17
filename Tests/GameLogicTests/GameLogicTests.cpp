dea8b3baa343f74f33589cf94f9773265b67fe60
Stefan Zwaal <zwalie@live.com>
2026-08-17 21:41:51 +0200

    Testing has been added

---
+++ Tests/GameLogicTests/GameLogicTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace GameLogicTests
+{
+	TEST_CLASS(GameLogicTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
